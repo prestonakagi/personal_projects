4378ccd9ea9bdfe6ec8c33d94bbb474b90a8481c
prestonakagi <preston.akagi@gmail.com>
2026-07-01 22:21:38 -0600

    Started metalborn flare method.

--- test_mistborn_simulation.py
+++ test_mistborn_simulation.py
@@ -28,11 +28,44 @@ def test_metalborn_burn():
     pusher3.burn(a_steel, anchor, radius_for_drag=0.825)
     expected_speed3 = anchor.anchor_mass / pusher3.body_mass
     assert pusher3.current_speed == expected_speed3
-    assert a_steel.remaining_mass == pytest.approx(0.7)  # TODO: 01JUL26, AssertionError. NEED to round floats to one decimal place; otherwise binary approximations yield repeated calculation errors.
+    assert a_steel.remaining_mass == pytest.approx(0.7)  # needed to round floats to one decimal place; otherwise binary approximations yield repeated calculation errors.
 
     pusher4.burn(a_steel, anchor, radius_for_drag=0.825)
     expected_speed4 = anchor.anchor_mass / pusher4.body_mass
     assert pusher4.current_speed == expected_speed4
     assert a_steel.remaining_mass == pytest.approx(0.6)
 
-test_metalborn_burn()
+# test_metalborn_burn()
+
+def test_metalborn_flare():
+    anchor = Anchor(anchor_mass=550.0, force_angle_degree=45.0)
+    a_steel = AlloSteel(initial_mass=1.3, remaining_mass=1.3, name_of_metal_key="Allo Steel")
+    # test all combos of simple and drag Booleans
+    pusher1 = Metalborn(initial_speed=0.0, current_speed=0.0, body_mass=62.0, want_simple_projectile=False, want_drag_projectile=False)
+    pusher2 = Metalborn(initial_speed=0.0, current_speed=0.0, body_mass=62.0, want_simple_projectile=True, want_drag_projectile=False)
+    pusher3 = Metalborn(initial_speed=0.0, current_speed=0.0, body_mass=62.0, want_simple_projectile=False, want_drag_projectile=True)
+    pusher4 = Metalborn(initial_speed=0.0, current_speed=0.0, body_mass=62.0, want_simple_projectile=True, want_drag_projectile=True)
+
+    pusher1.flare(a_steel, anchor, radius_for_drag=0.825) # average adult height divided by 2 in meters.
+    expected_speed1 = 5 * anchor.anchor_mass / pusher1.body_mass
+    assert pusher1.current_speed == expected_speed1
+    assert a_steel.remaining_mass == pytest.approx(0.91)
+
+    pusher2.flare(a_steel, anchor, radius_for_drag=0.825) # average adult height divided by 2 in meters.
+    expected_speed2 = 5 * anchor.anchor_mass / pusher2.body_mass
+    assert pusher2.current_speed == expected_speed2
+    assert a_steel.remaining_mass == pytest.approx(0.52)
+
+    pusher3.flare(a_steel, anchor, radius_for_drag=0.825) # average adult height divided by 2 in meters.
+    expected_speed3 = 5 * anchor.anchor_mass / pusher3.body_mass
+    assert pusher3.current_speed == expected_speed3
+    assert a_steel.remaining_mass == pytest.approx(0.13)
+
+    a_steel.remaining_mass = 1.3
+    pusher4.flare(a_steel, anchor, radius_for_drag=0.825) # average adult height divided by 2 in meters.
+    expected_speed4 = 5 * anchor.anchor_mass / pusher4.body_mass
+    assert pusher4.current_speed == expected_speed4 # TODO: why assertionError, when a_steel.remaining_mass = 1.3 not at start of block?
+    assert a_steel.remaining_mass == pytest.approx(0.91)
+    # assert a_steel.remaining_mass == pytest.approx(-0.26) # TODO: try testing this
+
+test_metalborn_flare()
