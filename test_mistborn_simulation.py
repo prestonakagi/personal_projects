d69b0b482946d8856ed4f004a99758e82a29dedc
prestonakagi <preston.akagi@gmail.com>
2026-07-02 17:21:43 -0600

    Started Mistborn class use duralumin method test. On mistborn simulation file, saved each round() to its respective class attribute.

--- test_mistborn_simulation.py
+++ test_mistborn_simulation.py
@@ -1,5 +1,5 @@
 import pytest
-from mistborn_simulation import Metalborn, Anchor, AlloSteel
+from mistborn_simulation import Metalborn, Mistborn, Anchor, AlloSteel
 from projectile_motion_simple import simulate_projectile_motion
 
 # Unit tests for Metalborn.burn using pytest
@@ -69,4 +69,23 @@ def test_metalborn_flare():
     assert a_steel.remaining_mass == pytest.approx(0.91)
     # assert a_steel.remaining_mass == pytest.approx(-0.26) # TODO: try testing this
 
-test_metalborn_flare()
+# test_metalborn_flare()
+
+def test_mistborn_use_duralumin():
+    anchor = Anchor(anchor_mass=550.0, force_angle_degree=45.0)
+    a_steel = AlloSteel(initial_mass=1.3, remaining_mass=1.3, name_of_metal_key="Allo Steel")
+    # test all combos of simple and drag Booleans
+    pusher1 = Mistborn(initial_speed=0.0, current_speed=0.0, body_mass=62.0, want_simple_projectile=False, want_drag_projectile=False)
+    pusher2 = Mistborn(initial_speed=0.0, current_speed=0.0, body_mass=62.0, want_simple_projectile=True, want_drag_projectile=False)
+    pusher3 = Mistborn(initial_speed=0.0, current_speed=0.0, body_mass=62.0, want_simple_projectile=False, want_drag_projectile=True)
+    pusher4 = Mistborn(initial_speed=0.0, current_speed=0.0, body_mass=62.0, want_simple_projectile=True, want_drag_projectile=True)
+
+    pusher1.use_duralumin(a_steel, anchor, radius_for_drag=0.825) # average adult height divided by 2 in meters.
+    expected_speed1 = (10 * a_steel.remaining_mass / a_steel.initial_mass) * anchor.anchor_mass / pusher1.body_mass
+    assert pusher1.current_speed == pytest.approx(88.7)
+    # assert pusher1.current_speed == pytest.approx(expected_speed1)
+    assert a_steel.remaining_mass == pytest.approx(0.0)
+    
+    a_steel.remaining_mass = 1.0
+
+test_mistborn_use_duralumin()
