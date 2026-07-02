bb574a08c99eab37b9225e1cce2172388017ac0f
prestonakagi <preston.akagi@gmail.com>
2026-07-01 21:56:52 -0600

    In test file, used pytest.approx method for the floats to assert.

--- test_mistborn_simulation.py
+++ test_mistborn_simulation.py
@@ -17,22 +17,22 @@ def test_metalborn_burn():
     pusher1.burn(a_steel, anchor, radius_for_drag=0.825) # average adult height divided by 2 in meters.
     expected_speed1 = anchor.anchor_mass / pusher1.body_mass
     assert pusher1.current_speed == expected_speed1
-    assert a_steel.remaining_mass == 0.9
+    assert a_steel.remaining_mass == pytest.approx(0.9)
     
     # test if remaining metal mass will keep decreasing.
     pusher2.burn(a_steel, anchor, radius_for_drag=0.825)
     expected_speed2 = anchor.anchor_mass / pusher2.body_mass
     assert pusher2.current_speed == expected_speed2
-    assert a_steel.remaining_mass == 0.8
+    assert a_steel.remaining_mass == pytest.approx(0.8)
 
     pusher3.burn(a_steel, anchor, radius_for_drag=0.825)
     expected_speed3 = anchor.anchor_mass / pusher3.body_mass
     assert pusher3.current_speed == expected_speed3
-    assert a_steel.remaining_mass == 0.7  # TODO: 01JUL26, AssertionError. NEED to round floats to one decimal place; otherwise binary approximations yield repeated calculation errors.
+    assert a_steel.remaining_mass == pytest.approx(0.7)  # TODO: 01JUL26, AssertionError. NEED to round floats to one decimal place; otherwise binary approximations yield repeated calculation errors.
 
     pusher4.burn(a_steel, anchor, radius_for_drag=0.825)
     expected_speed4 = anchor.anchor_mass / pusher4.body_mass
     assert pusher4.current_speed == expected_speed4
-    assert a_steel.remaining_mass == 0.6
+    assert a_steel.remaining_mass == pytest.approx(0.6)
 
 test_metalborn_burn()
