628607256449c5a821ddda7f8156fdb51e958abe
prestonakagi <preston.akagi@gmail.com>
2026-08-17 21:32:56 -0600

    set up pusher_skimmer1 in test_jump method for burn and use stored weight. Also how args and parameters relate to different classes and methods.

--- test_mistborn_simulation.py
+++ test_mistborn_simulation.py
@@ -369,15 +369,22 @@ def test_jump_and_change_weight_already_stored_weight():
     f_iron = FeroIron(initial_mass=1.0, remaining_mass=1.0, name_of_metal_key="Fero Iron")
     f_iron2 = FeroIron(initial_mass=1.0, remaining_mass=1.0, name_of_metal_key="Fero Iron")
     # pusher_skimmer1 to test use stored weight
-    pusher_skimmer1 = Twinborn(a_steel, f_iron, initial_speed=0.0, current_speed=0.0, body_mass=62.0, want_simple_projectile=True, want_drag_projectile=True)
+    pusher_skimmer1 = Twinborn(a_steel, f_iron, initial_speed=0.0, current_speed=15.87, body_mass=62.0, want_simple_projectile=True, want_drag_projectile=True)
     # pusher_skimmer 2 to test store_weight_while_jumping
     pusher_skimmer2 = Twinborn(a_steel2, f_iron2, initial_speed=0.0, current_speed=0.0, body_mass=62.0, want_simple_projectile=False, want_drag_projectile=True)
     # body weight potential is default 10.0
     pusher_skimmer2.body_weight_potential = 2.0
 
-    pusher_skimmer1.use_stored_weight(f_iron, weight_fraction_to_store=0.1)
-    # pusher_skimmer1.jump_and_change_weight(f_iron, anchor, (1.65/2), 0.8, 0.05)
-    pusher_skimmer1.burn(a_steel, anchor, (165/2))
+    # pusher_skimmer1 to test using stored weight while jumping
+    # pusher_skimmer1.use_stored_weight(f_iron, weight_fraction_to_store=0.1)
+    pusher_skimmer1.jump_and_change_weight(a_steel, f_iron, anchor, time_to_change_weight= 0.2, fraction_stored_weight_to_use= 0.1)
+        # burn result be simulate function(0.855 jump and change weight default, 
+        # 62.0 Twinborn default, 15.87 Twinborn(Metalborn) karg, 
+        # 45.0 Anchor.force_angle_degree default, 
+        # 0.2 jump_and_change_weight method kwarg, 
+        # 0.1 same method fraction_stored_weight_to_use kwarg default (results in 1 for speed_change, cause default is 10 for Twinborn.body_weight_potential we)
+    # pusher_skimmer1.burn(a_steel, anchor, (165/2))
+    # pusher_skimmer2 to test storing weight while jumping
     pusher_skimmer2.store_weight(f_iron2, weight_fraction_to_store=0.1)
     pusher_skimmer2.jump_and_change_weight(a_steel2, f_iron2, anchor, (1.65/2), 0.8, 0.05)
 
